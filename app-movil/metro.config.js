59e20a0d75a8aed6e23aa6eed0feb5d0ea159401
proamarc <proamarc@hotmail.com>
2026-06-27 11:45:32 -0500

    fix(mobile): allow nested metro dependencies

--- app-movil/metro.config.js
+++ app-movil/metro.config.js
@@ -1,11 +1,10 @@
-const path = require('path');
+const fs = require('fs');
 const { getDefaultConfig } = require('expo/metro-config');
 
 const projectRoot = __dirname;
 const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = [projectRoot];
-config.resolver.nodeModulesPaths = [path.join(projectRoot, 'node_modules')];
-config.resolver.disableHierarchicalLookup = true;
+config.watchFolders = config.watchFolders.filter((folder) => fs.existsSync(folder));
+config.resolver.nodeModulesPaths = config.resolver.nodeModulesPaths.filter((folder) => fs.existsSync(folder));
 
 module.exports = config;
